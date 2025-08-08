6099b811497e89cafc269fb55a5de5df9df7771d
celestine <ndayambajecelestin0@gmail.com>
2025-08-08 18:26:19 +0200

    push frontend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+  images: {
+    unoptimized: true, // if you're using <Image> component
+  },
+  basePath: '/frontend',
 };
 
 export default nextConfig;
